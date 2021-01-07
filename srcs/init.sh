d5d573e72890626caa2dabb30f3d266b831f5e59
gauthier poulain <poulain.gauthier16@gmail.com>
2021-01-07 11:16:15 +0100

    install worldpress

--- srcs/init.sh
+++ srcs/init.sh
@@ -1,15 +1,17 @@
-./phpymadmininstall.sh
+./setup_phpymadmin.sh
+./setup_wordpress.sh
 
 service mysql start
-service php7.3-fpm start
-service nginx start
 
 cp ./default /etc/nginx/sites-enabled/default
 
-mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('bite');"
+mysql -u root -e "CREATE USER 'user'@'localhost' IDENTIFIED BY 'bite';
+GRANT ALL PRIVILEGES ON * . * TO 'user'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;"
+mysql -u root -e "CREATE DATABASE wordpress"
 
-service mysql restart
-service php7.3-fpm restart
-service nginx restart
+
+service php7.3-fpm start
+service nginx start
 
 tail -f /var/log/nginx/access.log -f /var/log/nginx/error.log
---
+++ srcs/setup_wordpress.sh
@@ -0,0 +1,9 @@
+cd /var/www/html
+wget https://fr.wordpress.org/latest-fr_FR.tar.gz
+printf "extracting wordpress ... \n"
+tar xvf latest-fr_FR.tar.gz > /dev/null
+rm latest-fr_FR.tar.gz
+
+cd wordpress
+
+cp /srcs/wp-config.php .
